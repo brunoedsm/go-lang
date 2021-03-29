a35ba792de4448d1af5b8d7a71ae931f6c59bd3a
bruno.monteiro@s-money.fr <bruno.monteiro@s-money.fr>
2021-03-29 13:00:45 +0200

    'Added_Simple_Apps'

---
+++ fundamentals/apps/measure_converter.go
@@ -0,0 +1,51 @@
+package main
+
+import(
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main(){
+
+	//program call arguments (slice)
+	if len(os.Args) < 3 {
+		fmt.Println("Use args: measure_converter {values} {unity}")
+		os.Exit(1)
+	}
+
+	unityOrigin := os.Args[len(os.Args) - 1]
+
+	valuesOrigin := os.Args[1 : len(os.Args) - 1]
+
+	var targetUnity string
+
+	if unityOrigin == "celsius" {
+		targetUnity = "fahrenheit"
+	}else if unityOrigin == "quilometros"{
+		targetUnity = "milhas"
+	}else{
+		fmt.Println("Unknown unity")
+	}
+
+	for i,v := range valuesOrigin{
+		sourceValue, err := strconv.ParseFloat(v,64)
+		if err != nil{
+			fmt.Printf("Incorrect value %s in %d position\n",v,i)
+			os.Exit(1)
+		}
+	
+	var targetValue float64
+
+	if unityOrigin == "celsius"{
+		targetValue = sourceValue * 1.8 + 32
+	}else{
+		targetValue = sourceValue / 1.60934
+	}
+
+	fmt.Printf("Operation result: %.2f in %s\n",targetValue,targetUnity)	
+
+	
+	}
+	
+}
